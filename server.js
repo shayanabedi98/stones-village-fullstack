2cbe16a3ef1e06bfb1e0ee1304d13fd34d5a4b74
shayanabedi98 <shayanabedi@hotmail.com>
2024-01-14 14:53:17 -0500

    prepare for deployment

--- server.js
+++ server.js
@@ -30,6 +30,6 @@ app.get("/inventory", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/dist", "index.html"));
 });
 
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.listen(port)
