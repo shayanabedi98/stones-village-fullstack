5ca4099b616a643ba34f1215e1ff99b9dec06d9f
shayanabedi98 <shayanabedi@hotmail.com>
2024-01-16 13:12:57 -0500

    attemping to fix pics not rendering in quartz

--- client/public/inventory.js
+++ client/public/inventory.js
@@ -411,7 +411,13 @@ const inventoryList = [
         {
           id: uuid(),
           name: "",
-          src: "./inventory/quartz/white/SVQ-05.jpg",
+          src: "/inventory/quartz/white/cat.jpeg",
+          alt: "",
+        },
+        {
+          id: uuid(),
+          name: "",
+          src: "/inventory/quartz/white/SVQ-05.jpg",
           alt: "",
         },
       ],
@@ -419,7 +425,7 @@ const inventoryList = [
         {
           id: uuid(),
           name: "",
-          src: "./inventory/quartz/grey/SVQ-06.jpg",
+          src: "/inventory/quartz/grey/SVQ-06.jpg",
           alt: "",
         },
       ],
@@ -427,7 +433,7 @@ const inventoryList = [
         {
           id: uuid(),
           name: "",
-          src: "./inventory/quartz/black/SVQ-03.jpg",
+          src: "/inventory/quartz/black/SVQ-03.jpg",
           alt: "",
         },
       ],
