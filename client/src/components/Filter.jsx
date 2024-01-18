d701aa640f67f53f34ca474aefcea9b20f877382
shayanabedi98 <shayanabedi@hotmail.com>
2024-01-17 20:07:01 -0500

    website reponsivenessthrough media queries in all pages

--- client/src/components/Filter.jsx
+++ client/src/components/Filter.jsx
@@ -1,40 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Filter = ({ handleFilter, filter }) => {
   return (
     <div className="filter">
-        <button
-          style={{ backgroundColor: filter.all && "#fedc74" }}
-          onClick={() => handleFilter("all")}
-        >
-          All
-        </button>
-        <button
-          style={{ backgroundColor: filter.onyx && "#fedc74" }}
-          onClick={() => handleFilter("onyx")}
-        >
-          Onyx
-        </button>
-        <button
-          style={{ backgroundColor: filter.porcelain && "#fedc74" }}
-          onClick={() => handleFilter("porcelain")}
-        >
-          Porcelain
-        </button>
-        <button
-          style={{ backgroundColor: filter.marble && "#fedc74" }}
-          onClick={() => handleFilter("marble")}
-        >
-          Marble
-        </button>
-        <button
-          style={{ backgroundColor: filter.quartz && "#fedc74" }}
-          onClick={() => handleFilter("quartz")}
-        >
-          Quartz
-        </button>
-      </div>
-  )
-}
+      <button
+        className="all"
+        style={{ backgroundColor: filter.all && "#fedc74" }}
+        onClick={() => handleFilter("all")}
+      >
+        All
+      </button>
+      <button
+        style={{ backgroundColor: filter.onyx && "#fedc74" }}
+        onClick={() => handleFilter("onyx")}
+      >
+        Onyx
+      </button>
+      <button
+        style={{ backgroundColor: filter.porcelain && "#fedc74" }}
+        onClick={() => handleFilter("porcelain")}
+      >
+        Porcelain
+      </button>
+      <button
+        style={{ backgroundColor: filter.marble && "#fedc74" }}
+        onClick={() => handleFilter("marble")}
+      >
+        Marble
+      </button>
+      <button
+        style={{ backgroundColor: filter.quartz && "#fedc74" }}
+        onClick={() => handleFilter("quartz")}
+      >
+        Quartz
+      </button>
+    </div>
+  );
+};
 
-export default Filter
+export default Filter;
